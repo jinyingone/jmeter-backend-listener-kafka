e3ff262e33274f373d7dbe053b3ff0a56045efd6
时尽营(社交产品中心) <jinyingshi@sohu-inc.com>
2025-01-21 16:18:31 +0800

    性能测试

--- src/test/java/io/github/rahulsinghai/jmeter/backendlistener/kafka/TestKafkaBackendClient.java
+++ src/test/java/io/github/rahulsinghai/jmeter/backendlistener/kafka/TestKafkaBackendClient.java
@@ -18,22 +18,51 @@ package io.github.rahulsinghai.jmeter.backendlistener.kafka;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.visualizers.backend.BackendListenerContext;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class TestKafkaBackendClient {
 
   private static KafkaBackendClient client;
+  private static BackendListenerContext context;
 
   @BeforeAll
-  public static void setUp() {
+  public static void setUp() throws Exception {
     client = new KafkaBackendClient();
+    Arguments defaultParameters = client.getDefaultParameters();
+    defaultParameters.removeArgument("kafka.bootstrap.servers");
+    defaultParameters.addArgument("kafka.bootstrap.servers", "");
+    defaultParameters.removeArgument("kafka.topic");
+    defaultParameters.addArgument("kafka.topic", "t1");
+    context = new BackendListenerContext(defaultParameters);
+    client.setupTest(context);
   }
 
   @Test
   public void testGetDefaultParameters() {
     Arguments args = client.getDefaultParameters();
     assertNotNull(args);
+  }
+
+  @Test
+  public void testhandleSampleResults() throws IOException {
+    List<SampleResult> results = new LinkedList<>();
+    for (int i = 0; i < 1000000; i++) {
+      SampleResult e = new SampleResult();
+      e.setBodySize(i);
+      results.add(e);
+    }
+    client.handleSampleResults(Collections.singletonList(new SampleResult()), context);
+
+    long s1 = System.currentTimeMillis();
+    client.handleSampleResults(results, context);
+    System.out.println(System.currentTimeMillis() - s1);
   }
 }
